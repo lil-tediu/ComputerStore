6e4b3234b4c29b2e8abd4dbdb4d7d2a08ab8b87c
Teodor <teodor.yolovski@gmail.com>
2020-06-06 14:43:54 +0300

    Added more tests for computer class

--- src/tests/TestsComputer.java
+++ src/tests/TestsComputer.java
@@ -12,7 +12,6 @@ import computer_store.Tablet;
 
 public class TestsComputer {
     private static Computer lenovo;
-    private static Computer corsair;
     private static Computer asus;
     private static Computer acer;
 	
@@ -25,9 +24,6 @@ public class TestsComputer {
 		acer = new Desktop( "Acer", "Nitro N50-600",  3000, 4, 16)
 				.withVideoRAMMemory(4)
 				.withPowerSupplyWatts(500);
-		corsair = new Desktop( "Corsair", "APCOGMR93900XN2080S",  3000, 4.6f, 32)
-				.withVideoRAMMemory(8)
-				.withPowerSupplyWatts(750);
 		lenovo = new Tablet("Lenovo", "Tab E8", 215, 1.3f, 1)
 				.withMemoryCardSlot(true)
 				.withOperatingSystem("Andorid");
@@ -43,6 +39,72 @@ public class TestsComputer {
 		Computer lenovoCopy = new Tablet("Lenovo", "Tab E8", 215, 1.3f, 1);
 		assertEquals(lenovo, lenovoCopy);
 	}
+	
+	@Test
+	public void testToStringTabletEquals() {
+		String lenovoPrinted = lenovo.toString();
+		String answer = "Tablet [manufacturer=" + "Lenovo" + ", model=" + "Tab E8" + 
+						", price=" + 215 + ", CPUSpeed=" + 1.3f + 
+						", RAMMemory=" + 1 + ", operatingSystem=" + "Andorid" + 
+						", storageInGb=" + 0 + ", hasMemoryCardSlot=" +
+						true + "] \n";
+		
+		assertEquals(lenovoPrinted, answer );
+	}
+	
+	@Test
+	public void testToStringTabletNotEquals() {
+		String lenovoPrinted = lenovo.toString();
+		String answer = "Tablet [manufacturer=" + "Samsung" + ", model=" + "SM-T295" + 
+						", price=" + 215 + ", CPUSpeed=" + 1.3f + 
+						", RAMMemory=" + 1 + ", operatingSystem=" + "Andorid" + 
+						", storageInGb=" + 0 + ", hasMemoryCardSlot=" +
+						true + "] \n";
+		
+		assertNotEquals(lenovoPrinted, answer );
+	}
+	
+	@Test
+	public void testToStringLaptopEquals() {
+		String asusPrinted = asus.toString();
+		String answer = "Laptop [manufacturer=" + "Asus" + ", model=" + "K50IJ" + 
+						", price=" + 1000 + ", CPUSpeed=" + 3.4f + 
+						", RAMMemory=" + 8 + ", colour=" + "Black" + ", screenSize=" + 15.4f
+						+ ", batteryCapacictymAh=" + 4200 + "] \n";
+		
+		assertEquals(asusPrinted, answer );
+	}
+	
+	@Test
+	public void testToStringLaptopNotEquals() {
+		String asusPrinted = asus.toString();
+		String answer = "Laptop [manufacturer=" + "Lenovo" + ", model=" + "Legion Y520" + 
+						", price=" + 1500 + ", CPUSpeed=" + 3.4f + 
+						", RAMMemory=" + 8 + ", colour=" + "Red" + ", screenSize=" + 17.4f
+						+ ", batteryCapacictymAh=" + 4200 + "] \n";
+		
+		assertNotEquals(asusPrinted, answer );
+	}
+	
+	@Test
+	public void testToStringDesktopEquals() {
+		String acerPrinted = acer.toString();
+		String answer = "Desktop [manufacturer=" + "Acer" + ", model=" + "Nitro N50-600" +
+				", price=" + 3000 + ", CPUSpeed=" + 4f + ", RAMMemory=" + 16 + 
+				", powerSupplyWatts=" + 500 + ", videoRAMMemory=" + 4 + "] \n";
+		assertEquals(acerPrinted, answer );
+	}
+	
+	@Test
+	public void testToStringDesktopNotEquals() {
+		String acerPrinted = acer.toString();
+		String answer = "Laptop [manufacturer=" + "Acer" + ", model=" + "Nitro N50-600" +
+				", price=" + 3000 + ", CPUSpeed=" + 4.0 + ", RAMMemory=" + 16 + 
+				", powerSupplyWatts=" + 500 + ", videoRAMMemory=" + 4 + "] \n";
+		assertNotEquals(acerPrinted, answer );
+	}
+
+
 
 
 }
